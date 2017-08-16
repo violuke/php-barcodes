1b89d5ec713005fefaa040af707406cb81bd0066
violuke <luke@viovet.co.uk>
2017-08-16 18:34:13 +0100

    Fix for some UPC/EAN barcodes

--- src/violuke/Barcodes/BarcodeValidator.php
+++ src/violuke/Barcodes/BarcodeValidator.php
@@ -70,7 +70,11 @@ class BarcodeValidator {
 				if (substr($this->gtin14, 6, 1) == 5) {
 					$this->type = self::TYPE_UPC_COUPON_CODE;
 				} else {
-					$this->type = self::TYPE_UPC;
+                    if (strlen($this->barcode) == 13) {
+                        $this->type = self::TYPE_EAN;
+                    } else {
+                        $this->type = self::TYPE_UPC;
+                    }
 				}
 			} elseif (substr($this->gtin14, 0, 6) == 0) {
 				// GTIN-14 code
